5cd364e336533d7faf6ef6040fc836c458c15a2a
Dmitry Teplyakov <dimon2242@gmail.com>
2019-03-17 18:21:34 +0300

    First implementation polygons rasterization method

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 #include "model.h"
 #include "tga.h"
 
+#include <unistd.h>
+
+//#define DEBUG
+
 #define WIDTH 800
 #define HEIGHT 800
 
 void swap(int *a, int *b);
 int iabs(int a);
 int sign(double x);
+
 
 /*
 * Using Bresenham's algorithm
@@ -41,9 +46,121 @@ Model *offsetModel(Model *model, double x, double y, double z) {
     return model;
 }
 
-void meshgrid(tgaImage *image, Model *model) {
-    tgaColor white = tgaRGB(255, 255, 255);
+void triangle(tgaImage *image, int x0, int y0, int x1, int y1, int x2, int y2, tgaColor color) {
+    if(y0 > y2) {
+        swap(&y0, &y2);
+        swap(&x0, &x2);
+    }
+    if(y0 > y1) {
+        swap(&y0, &y1);
+        swap(&x0, &x1);
+    }
+    if(y1 > y2) {
+        swap(&y1, &y2);
+        swap(&x1, &x2);
+    }
+    int yend; // End point of polygon
+    if((y0 < y1) &&  (y1 < y2)) {
+        yend = y1;
+    } else {
+        yend = y2;
+    }
 
+    #ifdef DEBUG
+    printf("yend y0 y1 y2 %d %d %d %d\n", yend, y0, y1, y2);
+    printf("DEBUG x0 x1 x2 %d %d %d\n", x0, x1, x2);
+    #endif
+
+    int xa, xb, yfinal;
+    for(int y = y0; y <= yend; y++) {
+        
+
+        if((y1 - y0) != 0) {
+            xa = x0 + (x1-x0) * (y - y0)/(y1 - y0);
+        } else {
+            xa = x0;
+        }
+        if((y2 - y0) != 0) {
+            xb = x0 + (x2-x0) * (y - y0)/(y2 - y0);
+        } else {
+            xb = x0;
+        }
+
+        int xstart, xend;
+        if(xa > xb) {
+            swap(&xa, &xb);
+        }
+        #ifdef DEBUG
+        printf("DEBUG xa xb %d %d\n", xa, xb);
+        #endif
+
+        for(int x = xa; x <= xb; x++) {
+            #ifdef DEBUG
+            printf("SET PIXEL X Y %d %d\n", x, y);
+            #endif
+            tgaSetPixel(image, x, y, color);
+        }
+        yfinal = y;
+    }
+    if((y0 < y1) &&  (y1 < y2)) {
+        y0 = yfinal;
+        y1 = yfinal;
+        x0 = xa;
+        x1 = xb;
+
+        if(y0 > y2) {
+            swap(&y0, &y2);
+            swap(&x0, &x2);
+        }
+        if(y0 > y1) {
+            swap(&y0, &y1);
+            swap(&x0, &x1);
+        }
+        if(y1 > y2) {
+            swap(&y1, &y2);
+            swap(&x1, &x2);
+        }
+        #ifdef DEBUG
+        printf("ADDITION x0 y0 %d %d; x1 y1 %d %d; x2 y2 %d %d\n", x0, y0, x1, y1, x2, y2);
+        #endif
+
+        for(int y = y0; y <= y2; y++) {
+        
+            if((y2 - y0) != 0) {
+                xa = x0 + (x2 - x0)*(y - y0)/(y2 - y0);
+            } else {
+                xa = x0;
+            }
+            if((y2 - y1) != 0) {
+                xb = x1 + (x2 - x1)*(y - y1)/(y2 - y1);
+                #ifdef DEBUG
+                printf("xb => y2 != y0 => %d\n", xb);
+                #endif
+            } else {
+                xb = x0;
+            }
+
+            int xstart, xend;
+            if(xa > xb) {
+                swap(&xa, &xb);
+            }
+            #ifdef DEBUG
+            printf("DEBUG ADDITION xa xb %d %d\n", xa, xb);
+            #endif
+
+            for(int x = xa; x <= xb; x++) {
+                #ifdef DEBUG
+                printf("SET PIXEL ADDITION X Y %d %d\n", x, y);
+                #endif
+                tgaSetPixel(image, x, y, color);
+            }
+        }
+    }
+}
+
+
+void meshgrid(tgaImage *image, Model *model, char *argv) {
+    tgaColor white = tgaRGB(255, 255, 255);
     for (unsigned i = 0; i < model->nface; ++i) {
         int screen_coords[3][2];
         for (unsigned j = 0; j < 3; ++j) {
@@ -55,10 +172,25 @@ void meshgrid(tgaImage *image, Model *model) {
         for (unsigned j = 0; j < 3; ++j) {
             BRLine(image, screen_coords[j][0],screen_coords[j][1],
                 screen_coords[(j+1)%3][0], screen_coords[(j+1)%3][1],white);
+            
         }
+        int j = 0;
+        tgaColor randColor = tgaRGB(rand() % 255, rand() % 255, rand() % 255);
+        triangle(image, screen_coords[j][0],screen_coords[j][1],
+            screen_coords[(j+1)%3][0], screen_coords[(j+1)%3][1],
+            screen_coords[(j+2)%3][0], screen_coords[(j+2)%3][1], randColor);   
+    }
+    
+    for (unsigned i = 0; i < model->nface; ++i) {
+        int screen_coords[3][2];
+        for (unsigned j = 0; j < 3; ++j) {
+            Vec3 *v = &(model->vertices[model->faces[i][3*j]]);
+            screen_coords[j][0] = ((*v)[0] + 1) * image->width / 2;
+            screen_coords[j][1] = (1 - (*v)[1]) * image->height / 2;
+        }
+        
     }
 }
-
 
 int main(int argc, char **argv)
 {
@@ -71,12 +203,13 @@ int main(int argc, char **argv)
     tgaImage *image = tgaNewImage(HEIGHT, WIDTH, RGB);
     Model *model = loadFromObj(argv[1]);
 
+ 
     if(argc > 6) {
         scaleModel(model, strtod(argv[3], NULL));
         model = offsetModel(model, strtod(argv[4], NULL), strtod(argv[5], NULL), strtod(argv[6], NULL));
     }
 
-    meshgrid(image, model);
+    meshgrid(image, model, argv[2]);
 
     if (-1 == tgaSaveToFile(image, argv[2])) {
         perror("tgaSateToFile");
@@ -86,6 +219,8 @@ int main(int argc, char **argv)
     tgaFreeImage(image);    
     return rv;
 }
+
+
 
 void BRLine(tgaImage *image, 
            int x0, int y0,
